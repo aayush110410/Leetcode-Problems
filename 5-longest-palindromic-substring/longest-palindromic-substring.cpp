67abce22266d05995c812f1f1a4d852c3d8fac59
Aayush Sharma <csai23039@glbitm.ac.in>
2026-01-26 12:04:36 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (62.15%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,33 +1,24 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int n = s.size();
-        vector<vector<bool>> dp(n, vector<bool>(n));
-        array<int, 2> ans = {0, 0};
-
-        for (int i = 0; i < n; ++i) {
-            dp[i][i] = true;
+       int n = s.size();
+       int start = 0, bestLen = 1;
+       int i = 0;
+    while (i < n){
+        int l = i, r = i;
+        while (r + 1 < n && s[r + 1] == s[i])
+            r++;
+        i = r + 1;
+        while (l - 1 >= 0 && r + 1 < n && s[l - 1] == s[r + 1]){
+            l--;
+            r++;
         }
-
-        for (int i = 0; i < n - 1; ++i) {
-            if (s[i] == s[i + 1]) {
-                dp[i][i + 1] = true;
-                ans = {i, i + 1};
-            }
+        if (r - l + 1 >bestLen){
+            bestLen = r - l + 1;
+            start = l;
         }
-
-        for (int diff = 2; diff < n; ++diff) {
-            for (int i = 0; i < n - diff; ++i) {
-                int j = i + diff;
-                if (s[i] == s[j] && dp[i + 1][j - 1]) {
-                    dp[i][j] = true;
-                    ans = {i, j};
-                }
-            }
-        }
-
-        int i = ans[0];
-        int j = ans[1];
-        return s.substr(i, j - i + 1);
+        
+    }
+    return s.substr(start, bestLen);
     }
 };
